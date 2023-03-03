9b78c3527fb7095fe8a10f300ffe68dfe814937d
Maksim_Vasilkou <maksim_vasilkou@epam.com>
2023-03-03 20:44:48 +0300

    dynamodb integration done, post method created, DB filled with mock data and endpoint to create works fine

--- cars-ecmascript-server/productsList.js
+++ cars-ecmascript-server/productsList.js
@@ -1,18 +1,44 @@
-import { products } from './mocks/mocks';
+import AWS from 'aws-sdk';
 
 export const getProductsList = async (event, context, callback) => {
   try {
+    console.log(`execution func getProductsList, Event = ${event}, context = ${context}, callback = ${callback}`);
+    const dynamodb = new AWS.DynamoDB.DocumentClient();
     const response = {
-      statusCode: 200,
       headers: {
         'Access-Control-Allow-Origin': '*',
       },
-      body: JSON.stringify(products),
     };
+    const { Items: products } = await dynamodb
+      .scan({
+        TableName: process.env.PRODUCT_TABLE,
+      })
+      .promise();
+
+    const { Items: stocks } = await dynamodb
+      .scan({
+        TableName: process.env.STOCK_TABLE,
+      })
+      .promise();
+
+    try {
+      const productsWithStocks = products.map((product) => {
+        const { count } = stocks.find((stock) => stock.product_id === product.id);
+        return {
+          ...product,
+          count
+        };
+      });
+      response.statusCode = 200;
+      response.body = JSON.stringify(productsWithStocks);
+    } catch (error) {
+      console.log('Error', error);
+      response.statusCode = 400;
+    }
 
     return callback(null, response);
-  } catch (error) {``
-    console.log('Error in getProductsList, Product not found ', error)
-    return callback(e);
+  } catch (error) {
+    console.log('Error in getProductsList', error);
+    return callback(error);
   }
 };
